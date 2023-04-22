2284192ffff2e956282c230c5675c684e17dcf83
HuyHieu <huyhieu.hust2855@gmail.com>
2023-04-23 01:54:45 +0700

    Create DBcontext, config, add DTO

---
+++ JwtAuthASPNet7WebAPI/Core/UpdatePermisstionDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthASPNet7WebAPI.Core
+{
+    public class UpdatePermisstionDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+    }
+}
